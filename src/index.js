13d8480de32db9b4c5b661f69c9aeb564c0b3954
Alexander Alexandrov <aalexandrovv@gmail.com>
2018-07-10 11:13:11 +0300

    Parallel combination operator

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { reactive } from './reactive';
 export { handle } from './handle';
+export { par } from './par';
 export { isFunction, isObject, isNil, isSame } from './is';
---
+++ src/par.spec.js
@@ -0,0 +1,24 @@
+import { of } from 'rxjs';
+import { map, bufferCount } from 'rxjs/operators';
+import { par } from './par';
+
+describe('par operator', () => {
+    it('should return operator (function)', () => {
+        const operator = par();
+        expect(operator).toBeInstanceOf(Function);
+    });
+
+    it('should com provided operators to apply transformations in parallel (merging resulting streams)', (done) => {
+        const mul = x => y => x * y;
+        const mul123 = par(map(mul(1)), map(mul(2)), map(mul(3)));
+        of(2)
+            .pipe(
+                mul123,
+                bufferCount(3),
+            )
+            .subscribe((values) => {
+                expect(values).toEqual(expect.arrayContaining([2 * 1, 2 * 2, 2 * 3]));
+                done();
+            });
+    });
+});
